02255e90099d18ed3de1109611cf10626aa93848
Franciane <franpp22@gmail.com>
2023-01-09 17:55:27 -0300

    quarto commit javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,20 @@ function showModal() {
     const element = document.querySelector('.modal')
     element.classList.add('showModal')
 }
+
+function hideModal() { 
+    const element = document.querySelector('.modal')
+    element.classList.remove('showModal')
+}
+
+const btn = document.querySelector('.copy')
+const textArea = document.querySelector('textarea')
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    navigator.clipboard.writeText(textArea.value)
+    alert('E-mail copiado para área de transferência!')
+
+    hideModal()
+})
